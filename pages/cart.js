2ce23473a4b22b2917d4660c60af44bb2c02e93b
IreAyo <confidenceigwe@gmail.com>
2021-07-07 12:29:22 +0100

    update

--- pages/cart.js
+++ pages/cart.js
@@ -115,7 +115,6 @@ const CategoryPage = ({ category }) => {
                                                 </td>
                                             </tr>))}
 
-
                                         </tbody>
                                     </table> 
                                     
